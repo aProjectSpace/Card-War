83e0b822d8bafca454194fb041fdad3187897c65
WhileAGO <39588368+WhileAGO@users.noreply.github.com>
2020-10-19 16:26:18 +0300

    Update mainFile.py

--- mainFile.py
+++ mainFile.py
@@ -19,6 +19,21 @@ class launchWindow(tk.Frame):
         intro.launchTimer(0)
         self.master.destroy()
 
+class gameWindow(tk.Frame):
+    def __init__(self, masterRoot=None):
+        super().__init__(masterRoot)
+        self.masterRoot = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+
+    def updates(self):
+
 root = tk.Tk()
-app = launchWindow(master=root)
-app.mainloop()
+lW = launchWindow(master=root)
+lW.mainloop()
+
+root2 = tk.Tk()
+gW = gameWindow(masterRoot=root2)
+gW.mainloop()
